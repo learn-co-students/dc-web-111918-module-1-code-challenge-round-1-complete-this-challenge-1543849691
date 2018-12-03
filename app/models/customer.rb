bd60984dd2e044858dcd503f776ce00aec656abf
github username <github email address>
2018-12-03 12:34:36 -0500

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,59 @@
 class Customer
-  attr_accessor :first_name, :last_name
-
+  attr_accessor :first_name, :last_name, :restaurant, :review
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @restaurant = restaurant
+    @review = review
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(name)
+    if self.first_name = self && self.last_name == self
+      return
+    end
+  end
+
+  def self.all_names
+    first_name_array = []
+    last_name_array = []
+    full_name_array = []
+    self[:first_name].all.each {|first| first.first_name == self}
+      first_name_array << self
+    self[:last_name].all.each {|last| last.last_name == self}
+      last_name_array << self
+    first_name_array.zip(last_name_array) << full_name_array
+    return full_name_array
+  end
+
+
+  def self.find_all_by_first_name(first_name)
+    first_name_array = []
+    self[:first_name].all.each {|first| first.first_name == self}
+      first_name_array << self
+    return first_name_array
+  end
+
+
+  def self.add_review(restaurant_obj, content, rating)
+    Review.new(restaurant_obj, content, rating, self)
+  end
+
+  def self.num_reviews
+    Review[:rating].select {|rating| rating.customer == self}
+  end
+
+  def self.restaurants
+    Review[:restaurant].select {|rest| rest.restaurant == self}
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
-  
-end
+  attr_accessor :restaurant, :rating, :content
+  attr_reader :customer
+  @@all = []
 
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    self[:customer].select {|cust| cust.customer == self}
+  end
+
+  def restaurant
+    self[:restaurant].select {|rest| rest.restaurant == self}
+  end
+
+  def rating
+    self[:rating].select {|num| num.rating == self}
+  end
+
+  def content
+    self[:content].select {|written_review| written_review.content == self}
+  end
+end
