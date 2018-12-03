2186815066f4956e9db14b9f39cd876ff137d77f
github username <github email address>
2018-12-03 12:51:51 -0500

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,60 @@
+# #### Build out the following methods on the `Restaurant` class
+#
+# - `Restaurant.all`
+#   - returns an array of all restaurants
+# - `Restaurant.find_by_name(name)`
+#   - given a string of restaurant name, returns the
+#   first restaurant that matches
+
 class Restaurant
   attr_accessor :name
 
+@@all = []
   def initialize(name)
     @name = name
+    self.class.all << self
   end
 
+  def self.all
+    @@all
+  end
+
+
+  def self.find_by_name(name)
+  #   - given a string of restaurant name, returns the
+  #   first restaurant that matches (.find?)
+  name = "Bukom Cafe"
+  Restaurant.all.find {|rest| rest.name == name}
+  end
+
+  #### Restaurant
+  #
+  # - `Restaurant#customers`
+  #   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+  # - `Restaurant#reviews`
+  #   - returns an array of all reviews for that restaurant
+  # - `Restaurant#average_star_rating`
+  #   - returns the average star rating for a restaurant based on its reviews
+  # - `Restaurant#longest_review`
+  #   - returns the longest review content for a given restaurant
+
+  def customers
+    arr = []
+    Review.all.each {|ele| arr << "#{ele.cust_obj.first_name} #{ele.cust_obj.last_name}"}
+    arr.uniq
+  end
+
+  def reviews(rest_name)
+    arr = []
+    Review.all.each select {|ele| ele.rest_obj.name == rest_name}
+  end
+
+  def longest_review
+    arr = []
+    Review.all.each {|ele| }
+
+  end
+
+
+      #####    END CLASS #######
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,38 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+## TEST RESTAURANTS ##
+# def initialize(name)
+
+r1 = Restaurant.new("Bukom Cafe")
+r2 = Restaurant.new("Bob's Burgers")
+r3 = Restaurant.new("Jollof Palace")
+r4 = Restaurant.new("Taqueria San Marcos")
+r5 = Restaurant.new("Bakersfield")
+
+
+
+## TEST CUSTOMERS ##
+# def initialize(first_name, last_name)
+
+ben = Customer.new("Benjamin", "Addai")
+jonnel = Customer.new("Jonnel", "Benjamin")
+anuj = Customer.new("Anuj", "Sharma")
+michael = Customer.new("Michael", "Jordan")
+shinik = Customer.new("Shinik", "Cupo")
+
+## TEST REVIEWS ##
+# attr_accessor :rest_obj, :cust_obj
+
+rev1 = Review.new(r1, ben, "This place is amazing!", 5)
+rev2 = Review.new(r2, jonnel, "This place sucks!", 1)
+rev3 = Review.new(r3, shinik, "This place has great vibes!", 4)
+rev4 = Review.new(r4, michael, "This place has the best drinks!", 3)
+rev5 = Review.new(r5, anuj, "This place is kinda okay!", 2)
+
+
+
+##$$$$$$$$ END TESTS  $$$$$$$##
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
