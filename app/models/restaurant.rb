8c1c864b49e48aa2012d45ff5177635af0e0deea
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-03 11:34:55 -0500

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,48 @@
 class Restaurant
+
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+def self.all
+    @@all
+end
+
+def self.find_by_name(name_string)
+  self.all.select do |restaurant|
+    restaurant.name == name_string
+  end
+end
+
+def customers
+  self.reviews.collect do |review|
+    review.customer
+  end.uniq
+end
+
+def reviews
+  Review.all.select do |review|
+    review.restaurant == self
+  end
+end
+
+def average_star_rating
+  review_arr = self.reviews.collect do |review|
+    review.rating
+  end
+  (review_arr.inject(0.0,:+))/review_arr.length
+end
+
+def longest_review
+  review_arr = self.reviews.collect do |review|
+    review.content
+  end
+  review_arr.sort_by{|cont| cont.length}[-1]
+end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,17 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+joe = Customer.new("Joe","Biden")
+james = Customer.new("James","Cook")
+joe2 = Customer.new("Joe","noname")
 
+olive_garden = Restaurant.new("olive garden")
+oak = Restaurant.new("oak restaurant")
+
+joe.add_review(olive_garden,"bla bla bla",3)
+joe.add_review(oak,"bla bla bla bla",3)
+james.add_review(oak,"bla bla",5)
+james.add_review(olive_garden,"bla",2)
+james.add_review(olive_garden,"bla bli",1)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
