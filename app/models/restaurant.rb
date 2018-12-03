634a995a48bf233ba04c098123d961e6419c74fa
github username <github email address>
2018-12-03 12:50:27 -0500

    Mod 1 Code Challenge: Joe Spinelli

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,58 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def customers
+    customers = []
+    Review.all.select do |review|
+      if review.restaurant == self
+        customers << review.customer
+      end
+    end
+    customers
+  end
+
+  def reviews
+    reviews = []
+    Review.all.select do |review|
+      if review.restaurant == self
+        reviews += ["name = #{review.customer.first_name} " "#{review.customer.last_name}", "rating = #{review.rating}", "content = #{review.content}"]
+      end
+    end
+    reviews
+  end
+
+  def average_star_rating
+    rating = []
+    Review.all.select do |review|
+      if review.restaurant == self
+        rating << review.rating
+      end
+    end
+    ave_rating = rating.inject(:+) / rating.size
+    ave_rating
+  end
+
+  def longest_review
+    Review.all.find do |review|
+      # if review.restaurant == self && review.content.length
+      # end
+    end
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
+  attr_accessor :customer, :rating, :restaurant, :content
+  @@all = []
+
+  def initialize(customer, rating, restaurant, content)
+    @customer = customer
+    @rating = rating
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
   
 end
-
